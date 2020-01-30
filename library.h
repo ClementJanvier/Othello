36187d6eabd537b54581dc67abf3312e396a717f
Thomy <45008581+thomasjavelle@users.noreply.github.com>
2020-01-30 10:50:34 +0100

    Update library.h

--- library.h
+++ library.h
@@ -6,4 +6,4 @@
 void initialiser_othellier(int othellier[N]); //Initialise l'othellier à 0;
 void config_othellier(int othellier[N]); // Mets en place la configuration de départ de l'initialiser_othellier
 int MAJ_othellier(int othellier[N],);
-tour(int othellier[N], joueur X, );
+tour(int othellier[N], joueur X, onsenfout);
