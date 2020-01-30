3ce30303bfc04d02de7c0a53446aa84e72177500
Thomy <45008581+thomasjavelle@users.noreply.github.com>
2020-01-30 10:32:13 +0100

    Create library.h

---
+++ library.h
@@ -0,0 +1,9 @@
+#include"stdio.h"
+#include"stdlib.h"
+
+#define N 8
+
+void initialiser_othellier(int othellier[N]); //Initialise l'othellier à 0;
+void config_othellier(int othellier[N]); // Mets en place la configuration de départ de l'initialiser_othellier
+int MAJ_othellier(int othellier[N],);
+tour(int othellier[N], joueur X, );
