925a78fa3b81229b6e947dc79f6f92f7ab822616
Thomy <45008581+thomasjavelle@users.noreply.github.com>
2020-02-13 13:49:10 +0100

    Update library.h

--- library.h
+++ library.h
@@ -1,13 +1,20 @@
-#include"stdio.h"
-#include"stdlib.h"
+/**
+*\file library.h
+*\author Martin Thomas Clément Achraf
+*\version 1.0
+*\date 29/01/2020
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
 
 #define N 8
 
 typedef struct joueur {
   char * pseudo;
-  int couleur_j; // 0 = noir, 1 = blanc
+  int couleur_j; // 1 = noir, 2 = blanc
   int nb_pions; // nombre de pions de sa couleur sur l'othellier
-  int nb_tour; // nomnbre de tour que le joueur a joué depuis le début de la partie
+  int nb_tour; // nombre de tour que le joueur a joué depuis le début de la partie
   int plus_gros_coup; //coup prenant le plus de pions adverses 
   int palmares [3]={0,0,0}; //victoire,nuls,défaites
 };
@@ -18,13 +25,28 @@ typedef struct pion {
   int PosY; // position en Y du pion
 };
 
-void initialiser_othellier(int othellier[N][M]); //Initialise le plateau de jeu à 0;
-void config_othellier(int othellier[N]); // Mets en place la configuration de départ de l'initialiser_othellier
-void afficher_otthelier(int othellier[N]);
+/*Fonctions relatives à la matrice */
+void initialiser_othellier(int othellier[N][N]); //Initialise le plateau de jeu à 0; FAIT
+void config_othellier(int othellier[N][N]); // Mets en place la configuration de départ de l'initialiser_othellier; FAIT
+void afficher_otthelier(int othellier[N][N]); //FAIT
+int dans_matrice(int x, int y); //Verifie si la position n'est pas en dehors de la matrice : renvoie 1 si oui, 0 sinon
+
+/*Création Profil Joueur */
+void choix_pseudo(); // FAIT
+void choix_cote(); // Choix de la couleur utilisée par les joueurs //FAIT
+
+/*Menu Démarrage */
+void affichage_menu();//Affiche le menu de jeu; FAIT
+void affichage_difficulté();//Affiche les difficultés possibles; FAIT
+
 
 /*Tour*/
 //on change le joueur de qui c'est le tour
-void affichage_coup(int othellier[N], joueur X);
-void choix_coup(int othellier[N])
-int MAJ_othellier(int othellier[N],);
+void affichage_coup(int othellier[N][N], joueur X); //affichage coups possibles
+int choix_coup(int othellier[N][N],pion pion_1); //Choix du coup effectué par le joueur, return les pos du pion à poser
+int MAJ_othellier(int othellier[N][N],pion pion_1,joueur X); // Grâce aux PosX et PosY et à l'id du joueur, on place le nouveau pion et on retourne les pions nécéssaires
+// Dans le fonction ci-dessus on appelera la fonction ci-dessous.
+void retourne(int othellier[N][N],joueur X, pion pion_1); // Cette fonction parcours les pions et grâce a la pos du dernier pion posé elle met à jour la matrice en retournant les pions si nécéssaires.
 
+/*JEU*/
+void jeu();//La fonction est la fonction qui gère le déroulement du jeu, l'enchainement de toutes les fonctions ci-des
